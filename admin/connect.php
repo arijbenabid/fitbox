6fe847dd33db999504e7b8166cee9b1e8361d6c0
unknown <benaidarij@gmail.com>
2023-08-07 20:51:17 +0200

    add

---
+++ admin/connect.php
@@ -0,0 +1,9 @@
+
+<?php
+
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "fit";
+ $con = new mysqli($dbhost, $dbuser, $dbpass,$db);
+ ?>
---
+++ connect.php
@@ -0,0 +1,11 @@
+
+<?php
+
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "fit";
+ $con = new mysqli($dbhost, $dbuser, $dbpass,$db);
+ ?>
+
+
